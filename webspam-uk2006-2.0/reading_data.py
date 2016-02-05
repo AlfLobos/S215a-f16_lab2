851c89ce92f2995455289db93f3bd07faa014bdd
Mark Velednitsky <marvel2010+github@gmail.com>
2016-02-05 09:34:14 -0800

    new hostnames and hostgraph links, data reading in python

--- webspam-uk2006-2.0/reading_data.py
+++ webspam-uk2006-2.0/reading_data.py
@@ -1,13 +1,27 @@
-f = open('webspam-uk2006-set2-labels.txt')
+spam_file_1 = 'webspam-uk2006-set1-labels.txt'
+spam_file_2 = 'webspam-uk2006-set2-labels.txt'
 
-scores = {}
 
-for i in range(1000):
-    temp = f.readline().split()
-    rating = int(100*float(temp[2]))
-    if rating in scores.keys():
-        scores[rating] += 1
-    else:
-        scores[rating] = 1
+def read_file(file_name):
 
-print scores
+    file_reader = open(file_name)
+
+    host_scores = {}
+
+    line_count = 0
+
+    for file_line in file_reader:
+        line_count += 1
+        host_classification = file_line.split()
+        if len(host_classification) >= 3:
+            host_rating = int(100*float(host_classification[2]))
+            if host_rating in host_scores.keys():
+                host_scores[host_rating] += 1
+            else:
+                host_scores[host_rating] = 1
+
+    return sorted(host_scores.items()), line_count
+
+
+print read_file(spam_file_1)
+print read_file(spam_file_2)
